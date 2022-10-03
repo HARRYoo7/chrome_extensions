187e01e73b8fe995d299e6de79b2b709554afef2
Harjeet Pal <harjeet@Harjeets-Pc.local>
2022-10-03 13:05:55 +0530

    added script file for handling timer

---
+++ water_alarm/script.js
@@ -0,0 +1,37 @@
+let intervalTimer;
+
+/**
+	helper method used to show alarm alert
+**/
+function alertMessage(){
+	alert("please drink water!!!");
+}
+
+/**
+	helper method to start the timer.
+**/
+function startTimer(){
+	if(!intervalTimer){
+		console.log("water alarm started")
+		intervalTimer = setInterval(alertMessage, 60000);
+	}
+}
+
+/**
+	helper method to end the running timer.
+**/
+function endTimer(){
+	console.log("water alarm ended")
+	clearInterval(intervalTimer);
+	intervalTimer = null;
+}
+
+/**
+	defined init method that have click eventlisteners.
+**/
+function init(){
+	document.getElementById("start").addEventListener("click", startTimer);
+	document.getElementById("end").addEventListener("click", endTimer);
+}
+
+init();
